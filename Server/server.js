4f87c32f6cf1ab397d9f042ce4867996806788a5
Taha Asif <139437563+Taha-Asif-313@users.noreply.github.com>
2024-06-25 20:53:41 -0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -8,11 +8,17 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 // Cors
+// CORS configuration options
 const corsOptions = {
-  origin: 'https://blog-master-xi.vercel.app',
+  origin: ['http://localhost:5000', 'https://blogmaster313.netlify.app'], // Specify allowed origins
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 };
+
+
 
 
 // Routes
@@ -24,7 +30,11 @@ connectDB();
 
 // MiddleWares
 app.use(express.json());
+// Use the CORS middleware with the configured options
 app.use(cors(corsOptions));
+
+// Handle preflight requests for all routes
+app.options('*', cors(corsOptions));
 app.use(cookieParser());
 app.use("/api/user", userRoute);
 app.use("/api/blog", blogRoute);

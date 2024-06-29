081b3e00d8304213429220174a71eba5e9d65791
Taha Asif <139437563+Taha-Asif-313@users.noreply.github.com>
2024-06-29 12:18:00 -0700

    Update blogsContext.jsx

--- client/src/Context/blogsContext.jsx
+++ client/src/Context/blogsContext.jsx
@@ -3,9 +3,10 @@ import React, { createContext, useState } from 'react'
 const BlogContext = createContext()
 
 const BlogProvider = ({children}) => {
-    const [blogs, setblogs] = useState([])
+    const [blogs, setblogs] = useState([]);
+        const [userBlogs, setuserBlogs] = useState([])
   return (
-    <BlogContext.Provider value={{blogs,setblogs}}>
+    <BlogContext.Provider value={{blogs,setblogs,userBlogs,setuserBlogs}}>
     {children}
     </BlogContext.Provider>
   )
